371c7156e49260f8e1c40fc64f945cd24e840e57
Patrik <Soboj500@gmail.com>
2022-11-19 02:51:36 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header
+        style={{
+          backgroundColor: '#ffffff',
+          boxSizing: 'border-box',
+          height: '5vh',
+          padding: 20,
+          boxShadow: '0px 4px 30px 6px rgba(0,0,0,0.75)',
+          position: 'relative',
+        }}
+      >
+        awdawdawd
       </header>
-    </div>
+      <body style={{ padding: 20 }}>dwadawd</body>
+    </>
   );
 }
 
